a853bb8b8244e3a97a18e4d40d326440e7ea07c6
Kevin Rojas Vargas <krojas@oc.ld.co.cr>
2016-06-27 13:21:02 -0600

    Initial Commit

---
+++ SignalR/Public/js/main.js
@@ -0,0 +1,58 @@
+﻿var jq = $(document);
+var connection = $.connection.tableHub;
+var lstPeople = [];
+
+// Init Table
+(function (jq) {
+
+    var $table = jq.find('#tblPeople');
+
+    $table.bootstrapTable({
+        data: lstPeople || [],
+        cache: false,
+        height: 510,
+        striped: false,
+        pagination: true,
+        pageSize: 10,
+        pageList: [10, 50, 100, 250, 350, 500],
+        search: true,
+        showColumns: false,
+        showRefresh: false,
+        minimumCountColumns: 2,
+        clickToSelect: true,
+        rowAttributes: function (row, index) { return { 'data-id': row.ID }; },
+        columns: [{
+            field: 'ID',
+            title: 'ID',
+        }, {
+            field: 'Name',
+            title: 'Name',
+        }, {
+            field: 'Age',
+            title: 'Age',
+        }]
+    });
+
+})(jq);
+
+// Client's methods
+connection.client.receiveNewRow = function (person) {
+    
+    var $table = jq.find('#tblPeople');
+    lstPeople.push(JSON.parse(person));
+    $table.bootstrapTable('load', lstPeople);
+
+    toastr.success('New Row', 'Someone Insert a new Row');
+};
+
+// Server's Methods
+jq.find('#btnInsert').click(function () {
+
+    var newRow = { I: 1, Name: 'New Row', Age: 100 };
+
+    connection.server.insertRow(newRow);
+
+});
+
+// Stablish the connection
+$.connection.hub.start();
---
+++ SignalR/TableHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Web.Script.Serialization;
+
+namespace SignalR
+{
+    public class TableHub : Hub
+    {
+        public void InsertRow(Person person)
+        {
+            Clients.AllExcept(Context.ConnectionId).receiveNewRow(new JavaScriptSerializer().Serialize(person));
+        }
+        public void UpdateRow(Person person)
+        {
+            Clients.AllExcept(Context.ConnectionId).receiveUpdatedRow(new JavaScriptSerializer().Serialize(person));
+        }
+        public void RemoveRow(int id)
+        {
+            Clients.AllExcept(Context.ConnectionId).receiveRemovedRow(id);
+        }
+    }
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
